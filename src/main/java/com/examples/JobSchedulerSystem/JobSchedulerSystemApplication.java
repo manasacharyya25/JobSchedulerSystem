cc06d0d3cf133f9687b2133f25f31b0551ef4fc7
Manas Acharyya <manas.acharyya.nits.cse18@gmail.com>
2021-05-30 16:23:30 +0530

    Spring Init - Web, JPA, H2, Apache Kafka, Lombok

---
+++ src/main/java/com/examples/JobSchedulerSystem/JobSchedulerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.examples.JobSchedulerSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSchedulerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSchedulerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examples/JobSchedulerSystem/JobSchedulerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examples.JobSchedulerSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSchedulerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
